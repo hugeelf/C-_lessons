6632584f5221d14f857448b8edc3373d090bd64c
iam23db <iam23db@yandex.ru>
2022-08-20 14:46:06 +0300

    recorsion and 2d arrays

---
+++ Examples/Example015_tables2d/Program.cs
@@ -0,0 +1,42 @@
+﻿// string[,] table = new string [2,5];// двумерный массив, с количестовм строк 2, столбцов 5
+// // table [0,0] table [0,1] table[0,2]
+// // table [1,0] table [1,1] table [1,2]
+// table [1,2] = "слово";
+// for (int rows = 0; rows<2; rows++)
+// {
+//     for (int columns = 0; columns <5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+
+void PrintArray(int[,] matr)
+{
+    for (int rows = 0; rows < matr.GetLength(0); rows++) //указываем количество строк через функционал нахождения длины массива (0)-обращаемся к количеству строк или к первому массиву
+    {
+        for (int columns = 0; columns < matr.GetLength(1); columns++)//указываем количество строк через функционал нахождения длины массива (1)-обращаемся к количеству столбцов или ко второму массиву
+        {
+            Console.Write($"{matr[rows, columns]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int rows=0; rows<matr.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns<matr.GetLength(1); columns++)
+        {
+            matr[rows,columns] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Examples/Example017_factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿int FactorialNumber(int number)
+{
+    if (number == 1)
+    {
+        return 1;
+    }
+    else
+    {
+       return number * FactorialNumber(number - 1);
+    }
+}
+
+Console.WriteLine(FactorialNumber(123));
